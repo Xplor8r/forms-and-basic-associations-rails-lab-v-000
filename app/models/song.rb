7a2f7da135423523d62e708ba0a36181fcd6d594
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-02 00:53:05 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,8 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
-
+  has_many :notes
+  
   def genre_name=(name)
     self.genre = Genre.find_or_create_by(name: name)
   end
@@ -24,4 +25,7 @@ class Song < ActiveRecord::Base
       self.notes << note
     end
   end
+
+  def note_ids
+    self.notes ? self.
 end
