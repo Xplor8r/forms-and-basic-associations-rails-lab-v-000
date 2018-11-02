50d732346995f1949fe22515b0c8d267dd5420d2
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-02 01:17:42 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -47,6 +47,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name, :genre_id, s: [])
+    params.require(:song).permit(:title, :genre_id, :artist_name, s: [])
   end
 end
