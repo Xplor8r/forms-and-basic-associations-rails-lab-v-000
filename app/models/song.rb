d90ec209c5915988826a769d9b074a793aa1844d
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-02 01:21:09 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,7 +4,8 @@ class Song < ActiveRecord::Base
   has_many :notes
 
   def genre_name=(name)
-    self.genre = Genre.find_or_create_by(name: name)
+    genre = Genre.find_or_create_by(name: name)
+    self.genre = genre
   end
 
   def genre_name
