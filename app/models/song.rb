d5af648828bd135265e5a005ddd43a5cd00dd4b3
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-02 00:20:06 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,12 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
 end
